b1f91a1f5bea341631cace5592d8960a29a2fcb1
Gakudō <gakudo.learn@gmail.com>
2026-07-31 11:43:05 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
-"use client";
-
 import { useState } from "react";
-import { useTranslation } from 'react-i18next';
+import i18n from '../i18n';
+import { useTranslation, I18nextProvider } from 'react-i18next';
 import { createFileRoute } from "@tanstack/react-router";
 import {
   GraduationCap,
